d0623fb0c6ea2a5160700e02a703edf3a23936f8
Moontonight1021 <81334293+Moontonight1021@users.noreply.github.com>
2022-05-04 13:35:29 -0700

    Update exchange_rate_app.py

--- exchange_rate_app.py
+++ exchange_rate_app.py
@@ -9,9 +9,20 @@ import re
 #app title
 st.title('Currency Exchange Tool')
 
-source_currency = st.selectbox("Source currency",('USD','CAD','JPY'))
+currency_name_df = pd.read_csv('physical_currency_list.csv')
+currency_codes = list(currency_name_df["currency code"])
+currency_names = list(currency_name_df["currency name"])
 
-target_currency = st.selectbox("Target currency",('USD','CAD','JPY'))
+d = {}
+
+for code, name in zip(currency_codes,currency_names):
+    d[name] = code
+
+source_currency_name = st.selectbox("Source currency",currency_names)
+source_currency = d[source_currency_name]
+
+target_currency_name = st.selectbox("Target currency",currency_names)
+target_currency = d[target_currency_name]
 
 #currency exchange formula
 url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={source_currency}&to_currency={target_currency}&apikey=ZCN351IDJC1IVE6E'
