97a44e1c4f9166ddf48cd7105c583b1a4a5c37ac
Moontonight1021 <81334293+Moontonight1021@users.noreply.github.com>
2022-05-03 17:08:15 -0700

    Update exchange_rate_app.py

--- exchange_rate_app.py
+++ exchange_rate_app.py
@@ -2,3 +2,17 @@
 
 import streamlit as st
 import pandas as pd
+import numpy as np
+import requests
+import re
+
+#app title
+st.title('Currency Exchange Tool')
+
+#currency exchange formula
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=ZCN351IDJC1IVE6E'
+
+r = requests.get(url)
+data = r.json()
+
+st.write(data)
