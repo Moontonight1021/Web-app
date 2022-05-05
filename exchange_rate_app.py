504f29dc458655cf61db2f148a0d8b26e70b0aa1
Moontonight1021 <81334293+Moontonight1021@users.noreply.github.com>
2022-05-04 18:09:04 -0700

    Update exchange_rate_app.py

--- exchange_rate_app.py
+++ exchange_rate_app.py
@@ -27,11 +27,25 @@ target_currency = d[target_currency_name]
 #currency exchange formula
 url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={source_currency}&to_currency={target_currency}&apikey=ZCN351IDJC1IVE6E'
 
+if(source_currency==target_currency):
+    st.subheader("Please choose different currencies!")
+else:
+    r = requests.get(url)
+    data = r.json()
 
-r = requests.get(url)
-data = r.json()
+    df = pd.json_normalize(data)
+    
+    st.table(df)
+    
+    rate = list(df['Realtime Currency Exchange Rate.5. Exchange Rate'])
 
-df = pd.json_normalize(data)
-
-
-st.table(df)
+    st.title("Converting between currencies")
+    source_value1 = st.number_input(source_currency,min_value = 0)
+    target_value1 = float(rate[0]) * float(source_value1)
+    st.write(" =  " + target_currency)
+    st.write(target_value1)
+    
+    source_value2 = st.number_input(target_currency,min_value = 0)
+    target_value2 = float(source_value2)/float(rate[0])
+    st.write(" =  " + source_currency)
+    st.write(target_value2)
